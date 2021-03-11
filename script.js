f98e9a58cb2f753de4b52a62d2cbe4cfb0d720b2
Mingwei Samuel <mingwei.samuel@gmail.com>
2021-03-11 15:36:00 -0800

    Add basic (broken) number input

--- script.js
+++ script.js
@@ -2,19 +2,23 @@ const NS_SVG = 'http://www.w3.org/2000/svg';
 
 const sudoku = document.getElementById('sudoku');
 const sudokuHighlights = document.getElementById('sudoku-highlights');
+const sudokuGivens = document.getElementById('sudoku-givens');
+const sudokuFilled = document.getElementById('sudoku-filled');
 
 let selecting = false;
 
 const selected = new Set();
-function select(x, y) {
+function select({ offsetX, offsetY }) {
+  const x = (offsetX / 100) | 0;
+  const y = (offsetY / 100) | 0;
   const id = xy2id(x, y);
   if (!selected.has(id)) {
     selected.add(id);
 
     const highlight = document.createElementNS(NS_SVG, 'use');
     highlight.setAttribute('href', '#highlight');
-    highlight.setAttribute('x', x);
-    highlight.setAttribute('y', y);
+    highlight.setAttribute('x', 100 * x);
+    highlight.setAttribute('y', 100 * y);
 
     sudokuHighlights.append(highlight);
   }
@@ -28,26 +32,44 @@ function unselect() {
 function xy2id(x, y) {
   return 9 * y + x;
 }
-// function id2xy(id) {
-//   return [ id % 9, (id / 9) | 0 ];
-// }
+function id2xy(id) {
+  return [ id % 9, (id / 9) | 0 ];
+}
 
 sudoku.addEventListener('mousedown', e => {
   if (!selecting) {
     selecting = true;
     if (!e.shiftKey && !e.ctrlKey)
       unselect();
+    select(e);
   }
 });
 
 sudoku.addEventListener('mousemove', e => {
-  if (selecting) {
-    const x = 100 * ((e.offsetX / 100) | 0);
-    const y = 100 * ((e.offsetY / 100) | 0);
-    select(x, y);
+  if (selecting)
+    select(e);
+});
+
+window.addEventListener('keydown', e => {
+  let num = Number(e.key);
+  if (Number.isNaN(num))
+    return;
+
+  for (const id of selected) {
+    const [ x, y ] = id2xy(id);
+
+    const el = document.createElementNS(NS_SVG, 'text');
+    el.textContent = '' + num;
+    el.setAttribute('class', 'filled');
+    el.setAttribute('x', 100 * x + 50);
+    el.setAttribute('y', 100 * y + 50);
+
+    sudokuFilled.appendChild(el);
   }
 });
 
 window.addEventListener('mouseup', e => {
   selecting = false;
 });
+
+// function setGiven('')

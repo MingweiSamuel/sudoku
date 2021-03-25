2bb933cba9364f726feab81c3a9a8b98308df825
Mingwei Samuel <mingwei.samuel@gmail.com>
2021-03-25 14:12:48 -0700

    Open games in new tabs, ensure refresh on hash change

--- src/js/share.ts
+++ src/js/share.ts
@@ -1,30 +1,29 @@
 import * as init from "./initialize";
 
+function cloneAndOpenGame(frozen: true | null, parent: string | null) {
+    const targetGame = init.database.ref('game').push();
+    targetGame.set({
+        parent,
+        frozen,
+        board: init.boardData.get(),
+    });
+    const win = window.open('#' + targetGame.key, '_blank');
+    if (null == win) {
+        alert('Failed to open window, check your popup settings, or manually open this URL: '
+            + window.location.href.replace(window.location.hash, '#' + targetGame.key));
+        throw Error('Failed to open window.');
+    }
+    win.focus();
+}
+
 const buttonShare = document.getElementById('button-share')! as HTMLButtonElement;
 init.authPromise.then(_user => {
-    buttonShare.addEventListener('click', _e => {
-        const targetGame = init.database.ref('game').push();
-        targetGame.set({
-            frozen: true,
-            board: init.boardData.get(),
-        });
-        const win = window.open('#' + targetGame.key, '_blank');
-        if (null == win) {
-            alert('Failed to open window.');
-            throw Error('Failed to open window,');
-        }
-        win.focus();
-    });
+    buttonShare.addEventListener('click', _e => cloneAndOpenGame(true, null));
 });
 
 // TODO: Note there is a bit of a race condition here, on when game data arrives....
 const buttonPlay = document.getElementById('button-play')! as HTMLButtonElement;
-buttonPlay.addEventListener('click', _e => {
-    const targetGame = init.database.ref('game').push();
-    targetGame.set({
-        parent: init.gameKey,
-        board: init.boardData.get(),
-    });
-    window.location.hash = '#' + targetGame.key;
-    window.location.reload();
-});
+buttonPlay.addEventListener('click', _e => cloneAndOpenGame(null, init.gameKey));
+
+// Always refresh when hash changes.
+window.addEventListener('hashchange', _e => window.location.reload());
